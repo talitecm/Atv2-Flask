2a7a249d04cea9fc97432df919d2336fa7cbf6cd
Talita Cavalcanti <107768582+talitecm@users.noreply.github.com>
2024-11-23 18:50:20 +0000

    vs-1.0

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/produtos')
+def produtos():
+    return render_template('produtos.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
