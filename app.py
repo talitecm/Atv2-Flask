d61e7f33cea3328f2baf691a7cadec67d1d93a77
Talita Cavalcanti <107768582+talitecm@users.noreply.github.com>
2024-12-07 02:48:46 +0000

    vs-final

--- app.py
+++ app.py
@@ -21,3 +21,27 @@ def aboutus():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/gphero4')
+def gphero4():
+    return render_template('gphero4.html')
+
+@app.route('/phantom4p')
+def phantom4p():
+    return render_template('phantom4p.html')
+
+@app.route('/batphantom4p')
+def batphantom4p():
+    return render_template('batphantom4p.html')
+
+@app.route('/awse40mm')
+def awse40mm():
+    return render_template('awse40mm.html')
+
+@app.route('/gultra47mm')
+def gultra47mm():
+    return render_template('gultra47mm.html')
+
+@app.route('/gwatch7')
+def gwatch7():
+    return render_template('gwatch7.html')
